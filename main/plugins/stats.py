3b323f0cb2820a1730e312b12474d6cc9db19080
ShauryaDon <155410780+ShauryaDon@users.noreply.github.com>
2024-04-25 15:28:10 +0530

    Update stats.py

--- main/plugins/stats.py
+++ main/plugins/stats.py
@@ -56,7 +56,7 @@ async def stats(event):
             f'Memory Total: {mem_t}\n'\
             f'Memory Free: {mem_a}\n'\
             f'Memory Used: {mem_u}\n'\
-            f'Powered by **__[Team SPY](https://t.me/dev_gagan)__**\n'
+            f'Powered by **__[Shaurya]**\n'
     
     await event.reply(f"{stats}")
    #duz.edit(stats)
