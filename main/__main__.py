508bdb79fade25f51a0e81a365b094a77b47f06e
ShauryaDon <155410780+ShauryaDon@users.noreply.github.com>
2024-04-25 14:22:04 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -18,7 +18,7 @@ botStartTime = time.time()
 
 
 print("Successfully deployed!")
-print("Bot Deployed : Team SPY")
+print("Bot Deployed : Shaurya Soni")
 
 if __name__ == "__main__":
     from . import bot
